6fe33fb40da78ee49591ff6d22f77b226b6c4243
benjinhua <benjinhua66@gmail.com>
2022-08-25 21:44:32 +0800

    day1：update

--- src/pages/FunctionComponent.tsx
+++ src/pages/FunctionComponent.tsx
@@ -3,7 +3,7 @@ import React,{useState,useEffect} from 'react'
 export default function FunctionComponent() {
   const [date,setDate] = useState(new Date());
   useEffect(()=>{
-    // 相当于componentdidMount，componentdidupdate，componentdidunmount
+    // 相当于componentDidMount，componentDidupdate，componentDidUnmount
     const timer = setInterval(()=>{
       setDate(new Date())
     },1000)
