b5ab8649ebc4721e84a68a570fc7bbf9c8c51c31
benjinhua <benjinhua66@gmail.com>
2022-09-06 21:53:05 +0800

    redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,24 @@
-import { createStoreHook } from "react-redux";
-import React, { Component } from 'react'
+import { createStore } from "react-redux";
+
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+let store = createStore(counterReducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch({ type: 'counter/incremented' })
+// {value: 1}
+store.dispatch({ type: 'counter/incremented' })
+// {value: 2}
+store.dispatch({ type: 'counter/decremented' })
+// {value: 1}
+
+
