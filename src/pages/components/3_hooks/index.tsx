6f415cf2904398ea0ceecfc92a9d413803e06650
benjinhua <benjinhua66@gmail.com>
2022-09-22 09:37:09 +0800

    update：useEffect

--- src/pages/components/3_hooks/index.tsx
+++ src/pages/components/3_hooks/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 
 // class Demo extends Component {
 //   render() {
@@ -10,6 +10,15 @@ import React, { Component, useState } from 'react'
 
 function Demo() {
   const [ count, setCount ] = useState<number>(0);
+
+  useEffect(()=>{
+    // 相当于componentDidMount
+
+    // return的函数相当于componentWillUnmount
+    return () => { // 组件卸载前执行
+
+    }
+  },[])
 
   function add() {
     // setCount(count + 1); 第一种写法
