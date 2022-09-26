e377076b05488efca81f1e5679027911c582b9b1
benjinhua <benjinhua66@gmail.com>
2022-09-26 21:54:40 +0800

    update：Fragment

---
+++ src/pages/components/4_Fragment/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+
+// Fragment用于替换无用div，效果相当于<></>，Fragment可以添加key值
+export default class index extends Component {
+  render() {
+    return (
+      <Fragment>
+        index
+      </Fragment>
+    )
+  }
+}
+
+
